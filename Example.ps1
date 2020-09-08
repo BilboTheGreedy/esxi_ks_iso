78e936e8bf7b07d2aacc7f5aff23501412dce3b0
BilboTheGreedy <daniel.rapp@live.se>
2020-09-08 02:00:30 +0200

    Added NTP Source

--- Example.ps1
+++ Example.ps1
@@ -2,6 +2,8 @@
 $VMHost = Add-VMH -Hostname "ESXi01.test.local"
 $VMHost | Set-RootPW -PlainText SuperSecret1 -Algorithm SHA512
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
+$VMHost | Add-NTPSource -NTPSource 10.10.10.8
+$VMHost | Add-NTPSource -NTPSource 10.10.11.8
 $vSwitch = $VMHost | Add-vSwitch -Name vSwitch1 
 $vSwitch | Add-NetworkAdapter -vmnic vmnic4
 $vSwitch | Add-NetworkAdapter -vmnic vmnic5
@@ -10,6 +12,8 @@ $vSwitch | Add-PortGroup -Name VCSA -VlanId 40
 $VMHost = Add-VMH -Hostname "ESXi02.test.local"
 $VMHost | Set-RootPW -PlainText SuperSecret2 -Algorithm SHA256
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
+$VMHost | Add-NTPSource -NTPSource 10.10.10.8
+$VMHost | Add-NTPSource -NTPSource 10.10.11.8
 $vSwitch = $VMHost | Add-vSwitch -Name vSwitch1 
 $vSwitch | Add-NetworkAdapter -vmnic vmnic4
 $vSwitch | Add-NetworkAdapter -vmnic vmnic5
@@ -18,6 +22,8 @@ $vSwitch | Add-PortGroup -Name VCSA -VlanId 40
 $VMHost = Add-VMH -Hostname "ESXi03.test.local"
 $VMHost | Set-RootPW -PlainText SuperSecret3 -Algorithm MD5
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
+$VMHost | Add-NTPSource -NTPSource 10.10.10.8
+$VMHost | Add-NTPSource -NTPSource 10.10.11.8
 $vSwitch = $VMHost | Add-vSwitch -Name vSwitch1
 $vSwitch | Add-NetworkAdapter -vmnic vmnic4
 $vSwitch | Add-NetworkAdapter -vmnic vmnic5
