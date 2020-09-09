b50ab9e24533408e5250a433fa6c61d25b9091e3
BilboTheGreedy <daniel.rapp@live.se>
2020-09-09 20:08:28 +0200

    Update Example.ps1

--- Example.ps1
+++ Example.ps1
@@ -4,11 +4,15 @@ $VMHost | Set-RootPW -PlainText SuperSecret1 -Algorithm SHA512
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
 $VMHost | Add-NTPSource -NTPSource 10.10.10.8
 $VMHost | Add-NTPSource -NTPSource 10.10.11.8
+$vSwitch = $VMHost | Add-vSwitch -Name vSwitch0 -ModifyExisting $True
+$vSwitch | Add-NetworkAdapter -vmnic vmnic1
+$vSwitch | Add-PortGroup -Name Ex-Mgmt -VlanId 10
 $vSwitch = $VMHost | Add-vSwitch -Name vSwitch1 
 $vSwitch | Add-NetworkAdapter -vmnic vmnic4
 $vSwitch | Add-NetworkAdapter -vmnic vmnic5
 $vSwitch | Add-PortGroup -Name DC -VlanId 10
 $vSwitch | Add-PortGroup -Name VCSA -VlanId 40
+
 $VMHost = Add-VMH -Hostname "ESXi02.test.local"
 $VMHost | Set-RootPW -PlainText SuperSecret2 -Algorithm SHA256
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
@@ -20,6 +24,7 @@ $vSwitch | Add-NetworkAdapter -vmnic vmnic5
 $vSwitch | Add-PortGroup -Name DC -VlanId 10
 $vSwitch | Add-PortGroup -Name VCSA -VlanId 40
 $VMHost = Add-VMH -Hostname "ESXi03.test.local"
+
 $VMHost | Set-RootPW -PlainText SuperSecret3 -Algorithm MD5
 $VMHost | Set-ManagementNetwork -IPAddress 192.168.88.120 -NetworkMask 255.255.255.0 -Gateway 192.168.88.1 -VlanId 99 -Nameservers 192.168.88.10,192.168.10.11
 $VMHost | Add-NTPSource -NTPSource 10.10.10.8
@@ -30,7 +35,7 @@ $vSwitch | Add-NetworkAdapter -vmnic vmnic5
 $vSwitch | Add-PortGroup -Name DC -VlanId 10
 $vSwitch | Add-PortGroup -Name VCSA -VlanId 40
 #Get-VMH "*" | Remove-VMH
-Set-Paths -ISOPath "$PSScriptRoot\VMware-VMvisor-Installer-6.7.0.update03-14320388.x86_64.iso" -WorkDirectory "$PSScriptRoot\source" -OutputDirectory "$PSScriptRoot\ISO"
+Set-Paths -ISOPath "$PSScriptRoot\VMware-VMvisor-Installer-201912001-15160138.x86_64.iso" -WorkDirectory "$PSScriptRoot\source" -OutputDirectory "$PSScriptRoot\ISO"
 Mount-ISO 
 Set-SourceFiles
 foreach ($VMH in (Get-VMH -Hostname "*").Hostname)
@@ -40,5 +45,5 @@ foreach ($VMH in (Get-VMH -Hostname "*").Hostname)
 
 }
 Dismount-DiskImage -DevicePath $ISO.DevicePath | Out-Null
+Out-Json | Out-File "$PSScriptRoot\Current-Config.json" -Encoding ascii
 Write-Host "Done :)"
-Out-Json | Out-File "Current-config.json" -Encoding ascii
